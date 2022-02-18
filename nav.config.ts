d3e5f878b409c2527c086eee442c6a5859168b8b
bdfd <bdfd2005@gmail.com>
2022-02-18 15:28:28 -0500

    update person config

--- nav.config.ts
+++ nav.config.ts
@@ -1,9 +1,17 @@
+/*
+ * @Author: BDFD
+ * @Date: 2022-02-18 15:23:46
+ * @LastEditTime: 2022-02-18 15:26:43
+ * @LastEditors: BDFD
+ * @Description: 
+ * @FilePath: \Nav_Page\nav.config.ts
+ */
 import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/bdfd/Nav_Page',
 
   // Deployment branch name
   // 部署分支
